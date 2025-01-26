78009115104285fbf98948d7ce67cdad2de5f529
mariojr12 <msilvacoelhojunior@gmail.com>
2025-01-26 15:00:47 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+
+ const btnAvancar=document.getElementById('btn-avancar')
+ const cartoes=document.querySelectorAll('.cartao');
+ let cartaoAtual=0;
+ 
+ btnAvancar.addEventListener('click',function(){
+  if(cartaoAtual===cartoes.length-1) return;
+
+
+    const cartaoSelecionado=document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+
+     cartaoAtual++;   
+cartoes[cartaoAtual].classList.add('selecionado');
+  });
+ 
+  const btnVoltar=document.getElementById('btn-voltar');
+
+  btnVoltar.addEventListener('click',function(){
+
+    if(cartaoAtual=== 0)return;
+
+      const cartaoSelecionado=document.querySelector('.selecionado');
+      cartaoSelecionado.classList.remove('selecionado');
+  
+       cartaoAtual--;   
+  cartoes[cartaoAtual].classList.add('selecionado');
+    });
